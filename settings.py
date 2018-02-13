86e418e9f88a94732610ffa47baa285fc6d9cfba
mohadese sayahian <m.sayahian.d@gmail.com>
2018-02-13 23:00:43 +0330

    Add main

---
+++ settings.py
@@ -0,0 +1,15 @@
+url = {
+     'register':'http://127.0.0.1:8000/register/',
+     'login':'http://127.0.0.1:8000/login/',
+     'gameq2clone':'http://127.0.0.1:8000/gameq2clone/',
+     'gameq4clone':'http://127.0.0.1:8000/gameq4clone/',
+     'gameq2is_end':'http://127.0.0.1:8000/gameq2is_end/',
+     'gameq2isvalidplayer':'http://127.0.0.1:8000/gameq2isvalidplayer/',
+     'gameq2isvalidvwall':'http://127.0.0.1:8000/gameq2isvalidvwall/',
+     'gameq2isvalidhwall':'http://127.0.0.1:8000/gameq2isvalidhwall/',
+     'gameq2havewall':'http://127.0.0.1:8000/gameq2havewall/',
+     'gameq2pluswall':'http://127.0.0.1:8000/gameq2pluswall/',
+     'gameq2turn':'http://127.0.0.1:8000/gameq2turn/',
+     'gameq4':'http://127.0.0.1:8000/gameq4/',
+     'logout':'http://127.0.0.1:8000/logout/'
+}
